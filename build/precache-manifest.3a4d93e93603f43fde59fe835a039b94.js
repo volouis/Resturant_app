2680d8fe42ed130b3d7af76842ea60fd2992cb44
louisqvo <louisqvo@gmail.com>
2018-11-20 19:50:04 -0500

    update

---
+++ build/precache-manifest.3a4d93e93603f43fde59fe835a039b94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3036442776bed5837edf",
+    "url": "/Resturant_app/static/js/runtime~main.30364427.js"
+  },
+  {
+    "revision": "d2b635e1b0803c2d9bca",
+    "url": "/Resturant_app/static/js/main.d2b635e1.chunk.js"
+  },
+  {
+    "revision": "7c2cbd03eac759e31eab",
+    "url": "/Resturant_app/static/js/1.7c2cbd03.chunk.js"
+  },
+  {
+    "revision": "d2b635e1b0803c2d9bca",
+    "url": "/Resturant_app/static/css/main.e4390722.chunk.css"
+  },
+  {
+    "revision": "eadc73948cee46fc43ecd62b68e77cfb",
+    "url": "/Resturant_app/index.html"
+  }
+];
